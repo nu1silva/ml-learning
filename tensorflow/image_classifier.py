36885587c1bb1c1d0a6adc5ccc2945bcdd3ecfae
nu1silva <nu1silva@gmail.com>
2020-04-18 16:51:00 +0200

    tensorflow keras data pre-processing

--- tensorflow/image_classifier.py
+++ tensorflow/image_classifier.py
@@ -1,18 +1,41 @@
 import tensorflow as tf
+from tensorflow import keras
 
-mnist = tf.keras.datasets.mnist
+import numpy as np
+import matplotlib.pyplot as plt
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train, x_test = x_train / 255.0, x_test / 255.0
+print(tf.__version__)
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape=(28, 28)),
-  tf.keras.layers.Dense(128, activation='relu'),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10)
-])
+# load data
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-predictions = model(x_train[:1]).numpy()
-predictions
+# data classification
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
-tf.nn.softmax(predictions).numpy()
+# pre-process data
+print(train_images.shape)
+print(len(train_labels))
+print(train_labels)
+
+plt.figure()
+plt.imshow(train_images[0])
+plt.colorbar()
+plt.grid(False)
+plt.show()
+
+train_images = train_images / 255.0
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10, 10))
+for i in range(25):
+    plt.subplot(5, 5, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+
+# Building the model
